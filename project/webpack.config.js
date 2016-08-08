cc879460da143906d60cd4c567d260d3278c195e
phpusr <phpusr@gmail.com>
2016-08-08 17:56:55 +0500

    Webpack 2.5 Простая сборка - Окружение, NODE_ENV (https://www.youtube.com/watch?v=5XZqeuWkQ4o&list=PLDyvV36pndZHfBThhg4Z0822EEG9VGenn&index=6)

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
     entry: './home',
     output: {
@@ -7,12 +10,16 @@ module.exports = {
         library: 'home'
     },
 
-    watch: true,
+    watch: NODE_ENV == 'development',
 
     watchOptions: { // Опция для ускорения пересборки
         aggregateTimeout: 100 // default: 300
     },
 
-    devtool: 'eval'
+    devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+
+    plugins: [
+        new webpack.DefinePlugin({ NODE_ENV: JSON.stringify(NODE_ENV) })
+    ]
 
 };
--- project/welcome.js
+++ project/welcome.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(message) {
+
+    if (NODE_ENV == 'development') {
+        console.log(message);
+    }
+
+    alert(process.env.USER);
+
     alert(`Welcome ${message}`);
 };
