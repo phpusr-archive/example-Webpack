198b785b84b9a4366bcdd3d0e7b636d063fd348a
phpusr <phpusr@gmail.com>
2016-08-09 13:27:06 +0500

    Webpack 4.2: Продвинутые require - Объединение фрагментов (https://www.youtube.com/watch?v=Fy16GeWZ6Oc&index=18&list=PLDyvV36pndZHfBThhg4Z0822EEG9VGenn)

--- project-2/frontend/app.js
+++ project-2/frontend/app.js
@@ -5,6 +5,15 @@ document.getElementById('loginButton').onclick = function() {
     require.ensure([], function() {
         let login = require('./login');
         login();
-    });
+    }, 'auth');
+
+};
+
+document.getElementById('logoutButton').onclick = function() {
+
+    require.ensure([], function() {
+        let logout = require('./logout');
+        logout();
+    }, 'auth');
 
 };
