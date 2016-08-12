0f7c5036f4499ac9cbe39943ca5988df3310bf7f
phpusr <phpusr@gmail.com>
2016-08-12 17:30:46 +0500

    Webpack 8.1: Live Reload & HMR - Webpack Dev Server (https://www.youtube.com/watch?v=e9oA1AVV-jg&index=37&list=PLDyvV36pndZHfBThhg4Z0822EEG9VGenn)

---
+++ project-8/webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'frontend'),
+    entry: {
+        main: './main'
+    },
+    output: {
+        path: path.join(__dirname, 'public'),
+        publicPath: '/',
+        filename: '[name].js'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.styl']
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            include: path.join(__dirname, 'frontend'),
+            loader: 'babel?presets[]=es2015',
+        }, {
+            test: /\.jade$/,
+            loader: 'jade'
+        }, {
+            test: /\.styl$/,
+            loader: 'style!css!stylus?resolve url'
+        }, {
+            test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+            loader: 'file?name=[path][name].[ext]?[hash]'
+        }],
+    },
+
+    devServer: {
+        host: 'localhost', // default
+        port: 8080, // default
+        contentBase: path.join(__dirname, 'backend') // Директория откуда брать контент
+    }
+};
