a288e6ea5ada4dd5e1bb906aa943630c7b38df93
phpusr <phpusr@gmail.com>
2016-08-12 16:34:49 +0500

    Webpack 7.2 Длинное кеширование - Настройки для кеширования (https://www.youtube.com/watch?v=kxxFQZx3KOk&list=PLDyvV36pndZHfBThhg4Z0822EEG9VGenn&index=36)

--- project-7/webpack.config.js
+++ project-7/webpack.config.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const AssetsPlugin = require('assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const rimraf = require('rimraf');
+
+function addHash(template, hash) {
+    return NODE_ENV == 'production' ? template.replace(/\.[^.]+$/, `.[${hash}]$&`) : `${template}?hash=[${hash}]`;
+}
 
 module.exports = {
     context: path.join(__dirname, '/frontend'),
@@ -15,8 +22,8 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/public/assets'),
         publicPath: '/assets/',
-        filename: '[name].js',
-        chunkFilename: '[id].js',
+        filename: addHash('[name].js', 'chunkhash'),
+        chunkFilename: addHash('[id].js', 'chunkhash'),
         library: '[name]'
     },
 
@@ -35,10 +42,9 @@ module.exports = {
         }, {
             test: /\.styl$/,
             loader: ExtractTextPlugin.extract('css!stylus?resolve url')
-            //loader: 'style!css!stylus?resolve url'
         }, {
             test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-            loader: 'file?name=[path][name].[ext]'
+            loader: addHash('file?name=[path][name].[ext]', 'hash:6')
         }],
     },
 
@@ -48,9 +54,21 @@ module.exports = {
                 rimraf.sync(compiler.options.output.path);
             }
         },
-        new ExtractTextPlugin('[name].css', { allChunks: true }),
+        new ExtractTextPlugin(addHash('[name].css', 'contenthash'), { allChunks: true }),
         new webpack.optimize.CommonsChunkPlugin({ //TODO общие стили не выносятся
             name: 'common'
+        }),
+        new AssetsPlugin({
+            filename: 'assets.json',
+            path: __dirname + '/public/assets'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './about.html', // Выходной файл
+            chunks: ['common', 'about'] // Точки входа
+        }),
+        new HtmlWebpackPlugin({
+            filename: './home.html', // Выходной файл
+            chunks: ['common', 'home'] // Точки входа
         })
     ]
 };
